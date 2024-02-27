92e82624fa678744a71cf3d55c7c77b4e3f597f0
Harry C. Yan <134140985+hasAnybodySeenHarry@users.noreply.github.com>
2024-02-27 07:34:02 +0000

    client + room

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	room := NewRoom()
+	http.Handle("/", room)
+
+	go room.run()
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err.Error())
+	}
 }
