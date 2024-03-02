05c06d5a55805f587f564fb891a6e7e3063d754b
Harry C. Yan <134140985+hasAnybodySeenHarry@users.noreply.github.com>
2024-03-02 07:19:28 +0000

    template handler

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+type templateHandler struct {
+	filename string
+	templ    *template.Template
+	once     sync.Once
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
 
 func main() {
 	room := NewRoom()
-	http.Handle("/", room)
+	http.Handle("/room", room)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	go room.run()
 
